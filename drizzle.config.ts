8883464f425a09a56dc1d654e433c8f84a9da5a2
Francis Fish <f@francis.fish>
2025-11-12 16:27:29 +0000

    decomplicate drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,18 +8,12 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not set in the environment.");
 }
 
-const url = new URL(connectionString);
-
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    host: url.hostname,
-    port: Number(url.port || 5432),
-    user: url.username,
-    password: url.password,
-    database: url.pathname.slice(1),
+    url: process.env.DATABASE_URL!,
   },
 });
 
